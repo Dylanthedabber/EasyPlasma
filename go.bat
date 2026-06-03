8a02b46fb4c25198607ddbc5393f242f7e5f3dfc
dev <dev@localhost>
2026-06-03 14:08:39 -0500

    rewrite go.bat: sync, build, run

--- go.bat
+++ go.bat
@@ -2,26 +2,16 @@
 setlocal
 
 set SRC=Z:\public share\phantomrpc-poc-main\phantomrpc-poc-main\POCs\GreenPlasma
-set DST=C:\Users\unpriv\Music
+set BUILD=C:\Users\unpriv\Music
 
-if not "%~1"=="go" (
-    if not exist "%SRC%\easyplasma.cs" (
-        echo [-] Source not found: %SRC%
-        exit /b 1
-    )
-    robocopy "%SRC%" "%DST%" easyplasma.cs stub.c build.bat go.bat /IS /Z /COPY:DAT /W:1 /R:1 /NFL /NDL /NJH /NJS
-    copy /Y "%SRC%\go.bat" "%TEMP%\go_runner.bat" >nul
-    call "%TEMP%\go_runner.bat" go
-    exit /b
-)
+:: Sync latest source to build dir
+robocopy "%SRC%" "%BUILD%" easyplasma.cs stub.c build.bat /IS /Z /COPY:DAT /W:1 /R:1 /NFL /NDL /NJH /NJS
 
-cd /d "%DST%"
-call build.bat > "%TEMP%\build_out.tmp" 2>&1
-set BUILD_ERR=%errorlevel%
-type "%TEMP%\build_out.tmp"
-del "%TEMP%\build_out.tmp" >nul 2>&1
-if %BUILD_ERR% neq 0 (echo [-] Build failed & exit /b 1)
+:: Build
+cd /d "%BUILD%"
+call build.bat
+if errorlevel 1 exit /b 1
 
+:: Run
 echo.
-echo [*] Launching easyplasma.exe...
 easyplasma.exe
