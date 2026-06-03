d43d6b2f84944d2a8d1f2b9b1206dc12a6ff0e9b
dev <dev@localhost>
2026-06-03 14:10:02 -0500

    go.bat: resync self then re-run from temp

--- go.bat
+++ go.bat
@@ -4,14 +4,18 @@ setlocal
 set SRC=Z:\public share\phantomrpc-poc-main\phantomrpc-poc-main\POCs\GreenPlasma
 set BUILD=C:\Users\unpriv\Music
 
-:: Sync latest source to build dir
-robocopy "%SRC%" "%BUILD%" easyplasma.cs stub.c build.bat /IS /Z /COPY:DAT /W:1 /R:1 /NFL /NDL /NJH /NJS
+if "%~1"=="run" goto run
 
-:: Build
+:: Step 1: resync go.bat from share, then re-run fresh copy
+copy /Y "%SRC%\go.bat" "%TEMP%\go.bat" >nul
+call "%TEMP%\go.bat" run
+exit /b
+
+:run
+:: Step 2: sync source, build, run
+robocopy "%SRC%" "%BUILD%" easyplasma.cs stub.c build.bat /IS /Z /COPY:DAT /W:1 /R:1 /NFL /NDL /NJH /NJS
 cd /d "%BUILD%"
 call build.bat
 if errorlevel 1 exit /b 1
-
-:: Run
 echo.
 easyplasma.exe
